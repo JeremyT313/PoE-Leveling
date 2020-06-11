416fc2f20efbe13b793f53b7482d8e45fa7ebf54
JeremyT313 <xdetroit@live.com>
2020-06-10 22:40:07 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  {path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', component: },
+  { path: '', redirectTo: '', pathMatch: ''}
+]
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoputes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
